cb216ab9ac413090908d55247a7e738eaebee575
anton ratanen <noriss@kapsi.fi>
2019-01-26 09:35:47 +0200

    Init and day and night cycle

---
+++ GameJam19/Assets/Scripts/DayEditor/DayMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public List<GameObject> Furnices = new List<GameObject>();
+    public GameObject NightCharacter;
+    
+
+    private GameObject PickedObject;
+    private List<GameObject> PlacedOBjects = new List<GameObject>();
+    float timer = 0;
+    private Camera cam;
+    private bool picked = false;
+
+    public void SetFurnice(int index)
+    {
+        PickedObject = Instantiate(Furnices[index]);
+        picked = true;
+        timer = 1f;
+    }
+
+    public void ChangeToNight()
+    {
+        NightCharacter.SetActive(true);
+        foreach (var item in PlacedOBjects)
+        {
+            item.GetComponent<Collider>().enabled = true;
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizon = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(horizon * Time.deltaTime * speed, vertical * Time.deltaTime * speed, 0));
+
+        if (picked)
+        {
+            PickedObject.transform.position = cam.ScreenToWorldPoint(Input.mousePosition);
+            
+            if (Input.GetMouseButtonDown(0) && timer <= 0)
+            {
+                picked = false;
+                PlacedOBjects.Add(PickedObject);
+                if(Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out RaycastHit hit)){
+                    PickedObject.transform.position = hit.point + new Vector3(0, PickedObject.GetComponent<Collider>().bounds.extents.y, 0);
+
+                }
+            }
+            timer -= Time.deltaTime;
+        }
+    }
+}
---
+++ GameJam19/Assets/Scripts/DeadZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+
+    public Transform StartPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Contains("Player"))
+        {
+            other.gameObject.transform.position = StartPosition.position;
+        }
+    }
+}
